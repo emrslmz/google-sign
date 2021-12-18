3df0689aa53a54ee3ba54216969c1ec2b1e68fe2
emrslmz <emrslmz57@gmail.com>
2021-12-19 00:09:55 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h2>Google Sign</h2>
+      <img class="login-button" width="200px" src="../src/assets/google-login.png" alt="google-login-button" @click="login()" v-show="!isLogin" />
+      <div>
+        Login mi ? {{ isLogin }}
+        <br>
+        <p class="logout-button" @click="logOut()" v-show="isLogin">Çıkış Yap</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      isLogin: false,
+    };
+  },
+  methods: {
+    async logOut() {
+      const result = await this.$gAuth.signOut();
+      this.isLogin = false;
+      console.log(`result`, result);
+    },
+    async login() {
+      const googleUser = await this.$gAuth.signIn();
+      console.log("googleUser", googleUser);
+      console.log("getId", googleUser.getId());
+      console.log("getBaseProfile", googleUser.getBasicProfile());
+      console.log("getAuthResponse", googleUser.getAuthResponse());
+      console.log(
+          "getAuthResponse$G",
+          this.$gAuth.GoogleAuth.currentUser.get().getAuthResponse()
+      );
+      this.isLogin = this.$gAuth.isAuthorized;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.login-button {
+  cursor: pointer;
+}
+
+.logout-button {
+  cursor: pointer;
+  color: blue;
+  text-decoration: underline;
 }
 </style>
